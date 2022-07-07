23c1c6b3ccfa7974ef1deecc7a148bf1cde1c5ba
jenkio29 <60787571+jenkio29@users.noreply.github.com>
2022-07-07 17:43:11 -0400

    checking if push works

--- PCCchap8.py
+++ PCCchap8.py
@@ -31,13 +31,25 @@ describe_pet(animal_type='hamster', pet_name='harry')
 # def describe_pet(pet_name, animal_type='dog') <--set variable in function
 
 # Return Values
-def get_formatted_name(first_name, last_name, middle_name=''):
-    if middle_name:
-        full_name = first_name + ' ' + middle_name + ' ' + last_name
-    else:
-        full_name = first_name + ' ' + last_name
-    return full_name.title()
+# def get_formatted_name(first_name, last_name, middle_name=''):
+#   if middle_name:
+#      full_name = first_name + ' ' + middle_name + ' ' + last_name
+#    else:
+#       full_name = first_name + ' ' + last_name
+#    return full_name.title()
 
 
-musician = get_formatted_name('jimi', 'hendrix') # <---last argument optional
+# musician = get_formatted_name('jimi', 'hendrix') # <---last argument optional
+# print(musician)
+
+
+# Returning Dictionaries
+def build_person(first_name, last_name, age=''):
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+
+musician = build_person('jimi', 'hendrix', age='27')
 print(musician)
