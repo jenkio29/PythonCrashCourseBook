e6e8ef5a907a93ec68d32e99017724111673e184
jenkio29 <60787571+jenkio29@users.noreply.github.com>
2022-07-07 11:57:53 -0400

    checking if push works

--- PCCchap8.py
+++ PCCchap8.py
@@ -19,4 +19,6 @@ def describe_pet(animal_type, pet_name):
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
-describe_pet('hamster', 'harry')
+describe_pet('hamster', 'harry') # <--- arguments in order
+describe_pet('harry', 'hamster') # <--- arguments out of order
+
