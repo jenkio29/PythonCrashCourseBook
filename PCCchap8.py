aea070bdeaf6624d58d1b3b65511740bf9ac27bb
jenkio29 <60787571+jenkio29@users.noreply.github.com>
2022-07-07 11:53:03 -0400

    checking if push works

--- PCCchap8.py
+++ PCCchap8.py
@@ -14,3 +14,9 @@ def greet_user(username):
 greet_user('jesse')
 
 
+# Positional Arguments
+def describe_pet(animal_type, pet_name):
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+describe_pet('hamster', 'harry')
