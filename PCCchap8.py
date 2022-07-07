d7454e424ebf34282d3d6c31b8ca26f7708c3077
jenkio29 <60787571+jenkio29@users.noreply.github.com>
2022-07-07 14:48:44 -0400

    checking if push works

--- PCCchap8.py
+++ PCCchap8.py
@@ -22,3 +22,8 @@ def describe_pet(animal_type, pet_name):
 describe_pet('hamster', 'harry') # <--- arguments in order
 describe_pet('harry', 'hamster') # <--- arguments out of order
 
+# Keyword Arguments
+describe_pet(animal_type='hamster', pet_name='harry')
+
+# Default values
+# def describe_pet(animal_type='dog', pet_name) <--set variable in function
